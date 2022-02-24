35502965e111a35bcde72b5e1cc7a0b42c740eb4
mathieu <mathieumithridate@adrar-formation.com>
2022-02-24 21:31:06 +0100

    update config bdd

--- utils/connectBdd.php
+++ utils/connectBdd.php
@@ -1,9 +1,6 @@
 <?php   
     //connexion à la base de données
     //à modifier en fonction de votre base de données
-    $bdd = 'crud';
-    $login = 'root';
-    $mdp = '';
-    $bdd = new PDO('mysql:host=localhost;dbname='.$mdp.'', ''.$login.'',''.$mdp.'', 
+    $bdd = new PDO('mysql:host=localhost;dbname=crud', 'root','', 
     array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 ?>
